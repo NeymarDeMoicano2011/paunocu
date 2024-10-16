d03af2dcaf6203ed725f2a81fe58dda1a5d4f973
NeymarDeMoicano2011 <guilherme29lg@gmail.com>
2024-10-16 14:42:12 -0300

    Add files via upload

---
+++ flashcard-aula_8/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Qual a média do penis brasileiro?',
+    'O tamanho do penis do brasileiro é de 13,12 centimentros'
+)
+
+criaCartao(
+    'Duvidas',
+    'o Arthur Martinez Tesão é gay?',
+    'Depende do dia, mas a maioria é nos finais de semana a noite'
+)
+
+criaCartao(
+    'questionario',
+    'Só pode responder com sim ou não',
+    'Seu pai ja sabe que você é gay?'
+)
+
+criaCartao(
+    'Sabia dessa?',
+    'Quando é o periodo fértil de uma mulher',
+    'Nos dias que ela tem mais tesão'
+)
